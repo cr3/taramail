b1c23e6581d773420e060c6644734547f2167d1a
Marc Tardif <marc@interunion.ca>
2025-01-20 12:30:31 -0500

    Simplify Unbound service

--- unbound/entrypoint.sh
+++ unbound/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Receiving anchor key..."
-/usr/sbin/unbound-anchor -a /etc/unbound/trusted-key.key
+/usr/sbin/unbound-anchor -a /etc/unbound/root.key
 echo "Receiving root hints..."
 curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
 
