6136cd33aa64142db9afec7c7d060e398c8bc902
Marc Tardif <marc@interunion.ca>
2025-02-10 15:50:52 -0500

    Add db-model pytest fixture

--- taram/testing/db.py
+++ taram/testing/db.py
@@ -1,6 +1,7 @@
 """Taram database fixtures."""
 
 import pytest
+from attr import define, field
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker
@@ -68,3 +69,81 @@ def db_session(db_engine):
         session.close()
         transaction.rollback()
         connection.close()
+
+
+@define(frozen=True)
+class DbModel:
+    """Fixture for adding unique models to the database."""
+
+    session = field()
+    unique = field()
+
+    def __call__(self, model, **kwargs):
+        """Make a unique model and add it to the database.
+
+        This also happens to flush the database session so that the
+        default and server_default values can be realized.
+        """
+        kwargs = self._ensure_foreign_keys_exist(model, kwargs)
+
+        obj = self.unique("db-model", model, **kwargs)
+        self.session.add(obj)
+        self.session.flush()
+
+        return obj
+
+    def _ensure_foreign_keys_exist(self, model, kwargs):
+        """Make sure model can be created by pre-creating foreign objects."""
+        new_kwargs = kwargs.copy()
+
+        ignored_fkeys = self._relationship_covered_keys(model, kwargs) + list(kwargs.keys())
+
+        for fkey_constraint in model.__table__.foreign_key_constraints:
+            if not self._applicable(fkey_constraint, ignored_fkeys):
+                continue
+
+            table = fkey_constraint.referred_table
+            foreign_model = self._get_model_by_table_name(model, table.name)
+            foreign_object = self(foreign_model)
+
+            # Kwargs must reference the new foreign object
+            for local_key, remote_key in zip(fkey_constraint.column_keys, fkey_constraint.elements):
+                new_kwargs[local_key] = getattr(foreign_object, remote_key.column.name)
+
+        return new_kwargs
+
+    def _applicable(self, fkey_constraint, ignored_fkeys):
+        # Skip nullable columns
+        if all(col.nullable for col in fkey_constraint.columns):
+            return False
+
+        # Ignore manually set and relationship covered keys, the existance
+        # of the foreign object is the caller's responsibility
+        return all(col.name not in ignored_fkeys for col in fkey_constraint.columns)
+
+    def _relationship_covered_keys(self, model, kwargs):
+        """Find columns covered by relationships.
+
+        To avoid create a foreign instance one is already passed as a
+        relationship.
+        """
+        ignored_fkeys = []
+
+        for relationship in model.__mapper__.relationships:
+            if relationship.key in kwargs:
+                ignored_fkeys += [c.name for c in relationship.local_columns]
+
+        return ignored_fkeys
+
+    def _get_model_by_table_name(self, model, table_name):
+        for mapper in model.registry.mappers:
+            if getattr(mapper.class_, "__tablename__", None) == table_name:
+                return mapper.class_
+
+        raise ValueError(f"Table {table_name!r} not found in registry.")
+
+
+@pytest.fixture(scope="function")
+def db_model(db_session, unique):
+    """Database model fixture."""
+    return DbModel(db_session, unique)
--- tests/unit/testing/test_unique.py
+++ tests/unit/testing/test_unique.py
@@ -1,18 +1,96 @@
-"""Unit tests for the testing unique module."""
+"""Unit tests for the unique testing module."""
 
-from taram.testing.unique import unique_domain
+import enum
+
+import pytest
+from hamcrest import (
+    assert_that,
+    has_properties,
+    is_,
+)
+from sqlalchemy.types import (
+    BigInteger,
+    Boolean,
+    Enum,
+    Integer,
+    LargeBinary,
+    SmallInteger,
+    String,
+    Text,
+    Unicode,
+    UnicodeText,
+)
+
+from ...models import (
+    DefaultTest,
+    ForeignKeyTest,
+    NotNullableTest,
+    NullableTest,
+    ServerDefaultTest,
+)
 
 
 def test_unique_domain_default(unique):
     """Getting a unique domain should return a .com TLD by default."""
-    assert unique_domain(unique).endswith(".com")
+    assert unique("domain").endswith(".com")
 
 
 def test_unique_domain_tld(unique):
     """Getting a unique domain should be able to pass a `tld` argument."""
-    assert unique_domain(unique, tld="ca").endswith(".ca")
+    assert unique("domain", tld="ca").endswith(".ca")
 
 
 def test_unique_domain_twice(unique):
     """Getting a domain twice should not return the same value."""
-    assert unique_domain(unique) != unique_domain(unique)
+    assert unique("domain") != unique("domain")
+
+
+class StubEnum(enum.Enum):
+    """Stub enum."""
+
+    TEST = enum.auto()
+
+
+@pytest.mark.parametrize(
+    "column_type, matches",
+    [
+        (BigInteger(), is_(int)),
+        (Boolean(), True),
+        (Enum(StubEnum), StubEnum.TEST),
+        (Integer(), is_(int)),
+        (LargeBinary(), is_(bytes)),
+        (SmallInteger(), is_(int)),
+        (String(), is_(str)),
+        (Text(), is_(str)),
+        (Unicode(), is_(str)),
+        (UnicodeText(), is_(str)),
+    ],
+)
+def test_unique_db_value(column_type, matches, unique):
+    """A unique db-value should return a valid value."""
+    result = unique("db-value", column_type)
+
+    assert_that(result, matches)
+
+
+def test_unique_db_value_error(unique):
+    """An invalid db-value should raise a ValueError."""
+    with pytest.raises(ValueError):
+        unique("db-value", None)
+
+
+@pytest.mark.parametrize(
+    "model, matches",
+    [
+        (DefaultTest, has_properties(value=is_(None))),
+        (ForeignKeyTest, has_properties(value=is_(None))),
+        (NotNullableTest, has_properties(value=is_(str))),
+        (NullableTest, has_properties(value=is_(None))),
+        (ServerDefaultTest, has_properties(value=is_(None))),
+    ],
+)
+def test_unique_db_model(model, matches, unique):
+    """A unique db-model should return a valid model."""
+    result = unique("db-model", model)
+
+    assert_that(result, matches)
