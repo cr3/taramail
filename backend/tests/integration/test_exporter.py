570350e1986288f40d6767805cb7a9a5714c0138
Marc Tardif <marc@interunion.ca>
2025-12-05 11:45:20 -0500

    Fix exporter integration test by retrying

--- backend/tests/integration/test_exporter.py
+++ backend/tests/integration/test_exporter.py
@@ -1,19 +1,20 @@
 """Integration tests for the custom exporter."""
 
 from hamcrest import (
-    assert_that,
     has_items,
     has_properties,
 )
+from pytest_xdocker.retry import retry
 
 
 def test_exporter_check_rspamd(custom_exporter, rspamd_service):
     """The /check endpoint should chck Rspamd."""
-    metrics = custom_exporter.get_metrics("/check", params={
-        "rspamd_host": rspamd_service.ip,
-    })
+    def get_metrics(custom_exporter, rspamd_host):
+        return list(custom_exporter.get_metrics("/check", params={
+            "rspamd_host": rspamd_host,
+        }))
 
-    assert_that(metrics, has_items(
+    retry(get_metrics, custom_exporter, rspamd_service.ip).until(has_items(
         has_properties(
             name='mail_rspamd_scoring_check',
             samples=has_items(has_properties(value=1.0)),
