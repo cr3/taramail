c26f27eb07166a8739548e1d3c182d4db54059b7
Marc Tardif <marc@taram.ca>
2026-03-27 15:03:14 -0400

    Remove duplicate waiting lines in nginx/docker-entrypoint.sh

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-until ping taramail-sogo -c1 > /dev/null; do
-  echo "Waiting for SOGo..."
-  sleep 1
-done
-until ping taramail-rspamd -c1 > /dev/null; do
-  echo "Waiting for Rspamd..."
-  sleep 1
-done
-
 python3 /bootstrap.py
 
 exec "$@"
